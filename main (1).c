d7e99e9a4bcbf77310863dd66f83ba41b2211839
upsadri <adriiane.oliveira09@outlook.com>
2026-05-13 11:34:34 -0300

    menu utilizando while/ soma/ numero em dobro/ quadrado

--- main (1).c
+++ main (1).c
@@ -1,37 +1,36 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-  int opcao, numero, resultado;
-  while (opcao != 3){
+  int opcao = 0, numero, resultado;
+ 
+  while(opcao != 3){
       
-      printf("escolha numero");
-      scanf("%d", &numero);
-      
-      printf("===menu==\n");
-      printf("numero dobro\n");
-      printf("numero quadrado");
-      printf("sair\n");
+     printf("===menu==\n");
+      printf("1- numero dobro\n");
+      printf("2- numero quadrado\n");
+      printf("3- sair\n");
       
       printf("digite a opcao\n");
       scanf("%d", &opcao);
-      
+       
+       printf("escolha numero");
+      scanf("%d", &numero);
+     
       if(opcao == 1){
          resultado = numero * 2;
-          break;
+         printf("resuldo %d\n", resultado);
+         
       } else if(opcao == 2){
          resultado = numero * numero;
-          break;
+         printf("resuldo %d\n", resultado);
+          
       } 
-      printf("resuldo %d", resultado);
+      else{
+          printf("Deslogando...");
+      }
+     
       
 
   }
