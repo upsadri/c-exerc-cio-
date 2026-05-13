db0b5ea0a845015ce107f9a121c9bb101621a41b
upsadri <adriiane.oliveira09@outlook.com>
2026-05-12 21:40:41 -0300

    Add files via upload

--- main (1).c
+++ main (1).c
@@ -1,14 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 
-int main (){
-    char nome[50];
-    int idade;
-    float altura;
-    printf("digite seu nome ");
-    scanf ("%s", &nome);
-    printf ("digite sua idade ");
-    scanf ("%d", &idade);
-    printf("digite sua altura ");
-    scanf("%f", &altura);
+int main()
+{
+  int opcao, numero, resultado;
+  while (opcao != 3){
+      
+      printf("escolha numero");
+      scanf("%d", &numero);
+      
+      printf("===menu==\n");
+      printf("numero dobro\n");
+      printf("numero quadrado");
+      printf("sair\n");
+      
+      printf("digite a opcao\n");
+      scanf("%d", &opcao);
+      
+      if(opcao == 1){
+         resultado = numero * 2;
+          break;
+      } else if(opcao == 2){
+         resultado = numero * numero;
+          break;
+      } 
+      printf("resuldo %d", resultado);
+      
+
+  }
+
     return 0;
-}  
+}
--- main.c
+++ main.c
@@ -1,14 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 
-int main (){
-    int idade;
-    float altura;
-    
-    printf("Digite sua idade");
-    scanf("%d", &idade);
-    
-    printf("Digite sua altura: ");
-    scanf("%f", &altura);
-
-    return 0;
-}  
+int main()
+{
+   int i, numero;
+   printf("numeros pares de 1 a 20\n");
+   for(i=1; i<= 20; i++){
+       if(i % 2 == 0){
+           printf("%d\n", i);
+       }
+   }
+   printf ("===conta==\n");
+   printf ("digite um numero");
+   scanf ("%d", &numero);
+   for (int c = 0; c <= 10; c++){
+       printf ("%d * %d = %d\n", numero, c, numero * c);
+   }
+   return 0;
+}
